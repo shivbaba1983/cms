faed59c19acaccca79bf40975d981d48979a98db
--global <--global>
2025-03-21 15:10:04 -0400

    added-lord-ji

--- src/components/ComplexHeader.tsx
+++ src/components/ComplexHeader.tsx
@@ -1,9 +1,11 @@
 import './ComplexHeader.scss';
+import lord from './../assets/lord.jpg'
 const ComplexHeader =()=>{
 
     return(<div>
+  
         <div className="complex-headline">
-        <h1>शिव शक्ती संकुल, कोल्हार - बेलापूर रोड , कडीत खुर्द</h1>
+        <h1> <img src={lord} className="lord-image"/> शिव शक्ती संकुल, कोल्हार - बेलापूर रोड , कडीत खुर्द</h1>
         </div>
     </div>)
 }
--- src/components/CustomSlider.tsx
+++ src/components/CustomSlider.tsx
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import Sankul1 from './../assets/Sankul1.jpeg';
 import Sankul2 from './../assets/Sankul2.jpeg';
 import Sankul3 from './../assets/Sankul3.jpeg';
+import Sankul4 from './../assets/Sankul4.jpeg';
+import Sankul5 from './../assets/Sankul5.jpeg';
+import Sankul6 from './../assets/Sankul6.jpeg';
+import Sankul7 from './../assets/Sankul7.jpeg';
+import Sankul8 from './../assets/Sankul8.jpeg';
+import Sankul9 from './../assets/Sankul9.jpeg';
 const images = [
     Sankul1,
     Sankul2,
-    Sankul3
+    Sankul3,
+    Sankul4,
+    Sankul5,
+    Sankul6,
+    Sankul7,
+    Sankul8,
+    Sankul9
 ];
 
 const CustomSlider = () => {
